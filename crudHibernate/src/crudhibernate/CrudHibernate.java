2f716266d3c5abff01cdc32e570512e96ab30615
nazaMorillo <nazamorillo1@gmail.com>
2019-03-23 12:25:30 -0300

    proyecto hibernate

---
+++ crudHibernate/src/crudhibernate/CrudHibernate.java
@@ -0,0 +1,16 @@
+
+package crudhibernate;
+
+import java.time.Clock;
+
+/**
+ * @author Nazareno Morillo
+ */
+public class CrudHibernate {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo!");
+    }
+
+}
